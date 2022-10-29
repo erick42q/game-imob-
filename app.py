e4e52188aef425722757a51495f59ccc23005079
Erick Queiroz de Faria <erick42q@gmail.com>
2022-10-29 17:14:26 -0300

    create match system

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
+import random
 from cmath import polar
-from desafio.game import Players, Propriedades
 
+from desafio.game import Player, Propriedade, Match
+
+match = Match()
+
+# criar jogadores
 players = [
-    Players("inpulsivo"),
-    Players("exigente"),
-    Players("cauteloso"),
-    Players("aleatório"),
+    Player(0, "inpulsivo"),
+    Player(1, "exigente"),
+    Player(2, "cauteloso"),
+    Player(3, "aleatório"),
 ]
+random.shuffle(players)
 
+
+# criar propriedades
 propriedades = []
 
+for index in range(20):
+    propriedades.append(Propriedade())
 
-print(players[2])
+
+for id_round in range(50):
+    print(f"-----------------")
+    print(f"round: {id_round}")
+
+    player = players[0]
+    propriedade = propriedades[player.casa_atual]
+
+    match.roll_dice(player)
+    match.transaction(player, propriedade)
+
+    # print(player)
+    # print(propriedade)
+
+
+# radomisa ordem dos players
+# for player in players:
+#     round(player)
+
 
 
 
--- desafio/game.py
+++ desafio/game.py
@@ -1,19 +1,65 @@
-import itertools
+from random import  randint
 from dataclasses import dataclass
 
 
 @dataclass
-class Players:
+class Player:
+    id: int
     tipo: str
+    casa_atual: int = 0
     saldo: int = 100
 
+    def andar_casas(self, dado):
+        
+        if self.casa_atual+dado <= 19:
+            self.casa_atual += dado
+        else:
+            print("Volta no tabuleiro!!!")
+            self.casa_atual -= 19
+            self.saldo += 100
+            self.casa_atual += dado
 
-class Propriedades:
-    valor_de_venda: float = 200
+    def pagar(self, valor):
+        self.saldo -= valor
+        
+
+@dataclass
+class Propriedade:
+    dono: int = None
+    valor_venda: float = 200
     valor_aluguel: float = 20
-    player = int
+
+        
+class Match:
+
+    def roll_dice(self, player:dataclass):
+        steps = randint(0, 6)
+        casa_anterior = player.casa_atual
+        player.andar_casas(steps)
+        print(
+            f'''{player.tipo}: 
+            dado: {steps} 
+            casa anterior:  {casa_anterior}
+            casa atual: {player.casa_atual}
+            
+            saldo: {Player.saldo}'''
+        )
+
+    def transaction(self, player:dataclass, propriedade:dataclass):
+        
+        if propriedade.dono and propriedade.dono != player.id:
+            player.pagar(propriedade.valor_aluguel)
+            return
+
+        if player.saldo >= propriedade.valor_venda:
+            player.pagar(propriedade.valor_venda)
+            propriedade.dono = player.id
+            print(propriedade)
 
 
-class Partida:
-    def round():
-        pass
+        
+
+
+
+
+
