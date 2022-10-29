b3664a4e63dc5c0a7bf9508602368577f3b9e5bf
Erick Queiroz de Faria <erick42q@gmail.com>
2022-10-29 01:49:24 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from desafio.game import Players, Propriedades
+
+
+player_inpulsivo = Players(tipo="inpulsivo")
+player_exigente = Players(tipo="exigente")
+player_cauteloso = Players(tipo="cauteloso")
+player_aleatório = Players(tipo="aleatório")
+
+
+propriedades = Propriedades()
+
+print(vars(player_inpulsivo))
+
+
---
+++ desafio/game.py
@@ -0,0 +1,15 @@
+import itertools
+
+
+class Players:
+    pass
+
+
+class Propriedades:
+    pass
+
+
+class Partida:
+
+    def round():
+        pass
