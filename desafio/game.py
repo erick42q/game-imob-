e2800947b755a8bea8ef51b4eab21bffda62f26e
Erick Queiroz de Faria <erick42q@gmail.com>
2022-10-29 22:56:56 -0300

    gameover implemented

--- desafio/game.py
+++ desafio/game.py
@@ -1,5 +1,6 @@
-from random import  randint
-from dataclasses import dataclass
+from os import remove
+from random import  randint, randrange
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -14,52 +15,76 @@ class Player:
         if self.casa_atual+dado <= 19:
             self.casa_atual += dado
         else:
-            print("Volta no tabuleiro!!!")
             self.casa_atual -= 19
             self.saldo += 100
             self.casa_atual += dado
+            return "Volta no tabuleiro!!!"
 
     def pagar(self, valor):
         self.saldo -= valor
+
+
         
 
 @dataclass
 class Propriedade:
-    dono: int = None
-    valor_venda: float = 200
-    valor_aluguel: float = 20
+    valor_venda: int = field(init=False)
+    valor_aluguel: float = field(init=False)
+    owner: Player = None
 
-        
+    def __post_init__(self):
+        self.valor_venda = int(randrange(100,300))
+        self.valor_aluguel = self.valor_venda*0.2
+
+@dataclass    
 class Match:
+    
+    players: list[Player]
+
 
     def roll_dice(self, player:dataclass):
         steps = randint(0, 6)
         casa_anterior = player.casa_atual
-        player.andar_casas(steps)
+        msg=player.andar_casas(steps)
         print(
             f'''{player.tipo}: 
             dado: {steps} 
             casa anterior:  {casa_anterior}
+            {msg if msg is not None else ''}
             casa atual: {player.casa_atual}
             
-            saldo: {Player.saldo}'''
+            saldo: {player.saldo}
+            '''
         )
 
     def transaction(self, player:dataclass, propriedade:dataclass):
-        
-        if propriedade.dono and propriedade.dono != player.id:
+        if propriedade.owner and propriedade.owner != player:
             player.pagar(propriedade.valor_aluguel)
             return
-
         if player.saldo >= propriedade.valor_venda:
             player.pagar(propriedade.valor_venda)
-            propriedade.dono = player.id
+            propriedade.owner = player
             print(propriedade)
 
+    def game_over(self, player:dataclass):
+        if player.saldo <= 0:
+            self.players.remove(player)
+            print(f"player {player.tipo} não tem mais recursos e está fora do jogo")
 
-        
+    def winner(self):
+        if len(self.players) == 1:
+            return True
+        return False
 
+    def show_players(self):
+        msg = ''
+        for player in self.players:
+            msg += f'''
+                jogador: {player.tipo}
+                saldo: {player.saldo}            
+                '''
 
+        return msg
 
 
 
